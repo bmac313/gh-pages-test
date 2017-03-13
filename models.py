e60710e8d7594ab51b3cc69869b00376704292c8
Brendan <mcb0193@comcast.net>
2017-03-13 18:16:06 -0500

    Add some object models

--- models.py
+++ models.py
@@ -4,3 +4,14 @@ class User(db.Model):
 	username = db.StringProperty(required = True)
 	pw_hash = db.StringProperty(required = True)
 	email = db.StringProperty()
+	
+class Album(db.Model):
+	title = db.StringProperty(required = True)
+	artist = db.StringProperty(required = True)
+	year = db.StringProperty(required = True)
+	
+class Review(db.Model):
+	title = db.StringProperty(required = True)
+	author = db.ReferenceProperty(required = True) # reference to a User
+	body = db.TextProperty(required = True)
+	rating = db.StringProperty(required = True) # recommended, not recommended, or maybe
