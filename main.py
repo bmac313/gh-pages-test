8bccd1e147b30466501087367644d79e2c96e5df
Brendan <mcb0193@comcast.net>
2017-03-13 18:15:45 -0500

    Import hashutils

--- main.py
+++ main.py
@@ -17,7 +17,7 @@
 import webapp2, jinja2, os, re
 from google.appengine.ext import db
 from models import User
-#import hashutils
+import hashutils
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
